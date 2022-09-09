55ad71ca2e542bf7ca063001aa9a6e8f164d62f3
Andrey <dev.andr@yandex.com>
2022-09-09 21:25:30 +0500

    feat: dto for compilation added

---
+++ src/main/java/compilation/dto/NewCompilationDto.java
@@ -0,0 +1,26 @@
+package compilation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewCompilationDto {
+
+    private List<Integer> events;
+
+    private Boolean pinned;
+
+    @NotEmpty(message = "Title should not be empty")
+    @Size(min = 3, max = 100, message = "Title should less 100 and greater 3 characters")
+    private String title;
+
+}
