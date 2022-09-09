a9e8e2780591cd9b784acd619d5a0b269d460b3d
Andrey <dev.andr@yandex.com>
2022-09-09 21:06:44 +0500

    feat: dto for event added

---
+++ src/main/java/event/dto/AdminUpdateEventRequest.java
@@ -0,0 +1,44 @@
+package event.dto;
+
+import category.dto.CategoryDto;
+import lombok.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminUpdateEventRequest {
+
+    private String annotation;
+
+    private CategoryDto category;
+
+    private String description;
+
+    private String eventDate;
+
+    private Location location;
+
+    private Boolean paid;
+
+    private Integer participantLimit;
+
+    private Boolean requestModeration;
+
+    private String title;
+
+    @Builder
+    @Setter
+    @Getter
+    public static class Location{
+        private Double lat;
+        private Double lon;
+    }
+
+}
---
+++ src/main/java/event/dto/UpdateEventRequest.java
@@ -0,0 +1,37 @@
+package event.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateEventRequest {
+
+    @Size(min = 20, max = 2000, message = "Annotation should less 2000 and greater 20 characters")
+    private String annotation;
+
+    private Long category;
+
+    @Size(min = 20, max = 7000, message = "Description should less 2000 and greater 20 characters")
+    private String description;
+
+    private String eventDate;
+
+    private Boolean paid;
+
+    @Min(value = 1, message = "Participant limit should greater then 0")
+    private Integer participantLimit;
+
+    private Boolean requestModeration;
+
+    @Size(min = 3, max = 120, message = "Title should less 120 and greater 3 characters")
+    private String title;
+
+}
