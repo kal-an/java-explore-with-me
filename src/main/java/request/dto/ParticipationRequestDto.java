4ac20485b41698583a70c504da9f01cecbc0202c
Andrey <dev.andr@yandex.com>
2022-09-11 22:41:01 +0500

    feat: annotation added to ParticipationRequestDto

--- src/main/java/request/dto/ParticipationRequestDto.java
+++ src/main/java/request/dto/ParticipationRequestDto.java
@@ -1,15 +1,13 @@
 package request.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ParticipationRequestDto {
