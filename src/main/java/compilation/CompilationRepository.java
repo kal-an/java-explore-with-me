3acd51aa46016485fef7804894929eeffcb70275
Andrey <dev.andr@yandex.com>
2022-09-11 21:52:38 +0500

    feat: findAllByPinned added to CompilationRepository

--- src/main/java/compilation/CompilationRepository.java
+++ src/main/java/compilation/CompilationRepository.java
@@ -1,6 +1,15 @@
 package compilation;
 
+import compilation.dto.CompilationDto;
+import compilation.model.Compilation;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CompilationRepository extends JpaRepository<Compilation, Long> {
+import java.util.List;
+
+public interface CompilationRepository extends JpaRepository<Compilation, Integer> {
+
+    @Query("select c from Compilations c where c.pinned is ?1")
+    List<CompilationDto> findAllByPinned(Boolean pinned, Pageable pageable);
 }
