9486645241130d5272ab3b55f5e1a65de0c3f332
Andrey <dev.andr@yandex.com>
2022-09-11 21:57:31 +0500

    feat: annotation added to CategoryDto

--- src/main/java/category/dto/CategoryDto.java
+++ src/main/java/category/dto/CategoryDto.java
@@ -1,15 +1,13 @@
 package category.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Setter
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDto {
