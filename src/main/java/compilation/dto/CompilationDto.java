e7d47b12045441333c8b856e09259927f90d2322
Andrey <dev.andr@yandex.com>
2022-09-11 21:53:11 +0500

    feat: added annotation to CompilationDto

--- src/main/java/compilation/dto/CompilationDto.java
+++ src/main/java/compilation/dto/CompilationDto.java
@@ -1,9 +1,6 @@
 package compilation.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -13,6 +10,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CompilationDto {
 
     private Long id;
