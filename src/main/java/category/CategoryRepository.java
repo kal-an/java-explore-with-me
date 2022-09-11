6bd211a0bb8f03062f8babd19d8ffd36a3a187fd
Andrey <dev.andr@yandex.com>
2022-09-11 22:28:57 +0500

    feat: category repository added

---
+++ src/main/java/category/CategoryRepository.java
@@ -0,0 +1,8 @@
+package category;
+
+import category.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
