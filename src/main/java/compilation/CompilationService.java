2939f8b655bad1e511d44bf18369536be35dc9cf
Andrey <dev.andr@yandex.com>
2022-09-11 17:22:57 +0500

    feat: interface compilation service added

---
+++ src/main/java/compilation/CompilationService.java
@@ -0,0 +1,12 @@
+package compilation;
+
+import compilation.dto.CompilationDto;
+
+import java.util.List;
+
+public interface CompilationService {
+
+    List<CompilationDto> getCompilations(Boolean pinned, Integer from, Integer size);
+
+    CompilationDto getCompilation(Integer id);
+}
