2e73010cca14a3f8d43072d457282f290b177286
Andrey <dev.andr@yandex.com>
2022-09-12 15:00:56 +0500

    feat: create request repository

---
+++ src/main/java/request/RequestRepository.java
@@ -0,0 +1,11 @@
+package request;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import request.model.Request;
+
+import java.util.List;
+
+public interface RequestRepository extends JpaRepository<Request, Integer> {
+
+    List<Request> findByRequesterId(Integer id);
+}
