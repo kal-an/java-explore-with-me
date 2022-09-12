936d54f30fc13eb8145c10093865808d1d5f744c
Andrey <dev.andr@yandex.com>
2022-09-12 15:01:22 +0500

    feat: create request mapper

---
+++ src/main/java/request/RequestMapper.java
@@ -0,0 +1,23 @@
+package request;
+
+import request.dto.ParticipationRequestDto;
+import request.model.Request;
+
+public class RequestMapper {
+
+    public static ParticipationRequestDto toDto(Request request) {
+        return ParticipationRequestDto.builder()
+                .id(request.getId())
+                .created(request.getCreated())
+                .requester(request.getRequester().getId())
+                .status(request.getStatus().name())
+                .build();
+    }
+
+    public static Request toRequest(ParticipationRequestDto dto) {
+        return Request.builder()
+                .id(dto.getId())
+                .created(dto.getCreated())
+                .build();
+    }
+}
