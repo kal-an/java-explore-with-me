831243c3f10806f8b5f84845efab8caf7f515378
Andrey <dev.andr@yandex.com>
2022-09-12 22:47:30 +0500

    fix: change field id for category

--- src/main/java/category/dto/CategoryDto.java
+++ src/main/java/category/dto/CategoryDto.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 public class CategoryDto {
 
-    private Long id;
+    private Integer id;
 
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 1, max = 25, message = "Name should less 25 characters")
--- src/main/java/category/model/Category.java
+++ src/main/java/category/model/Category.java
@@ -17,7 +17,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false)
     private String name;
