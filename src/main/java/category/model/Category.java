a9236b831b8888d6616579adbb05332be05f244e
Andrey <dev.andr@yandex.com>
2022-09-15 16:00:02 +0500

    feat: add unique property to category name

--- src/main/java/category/model/Category.java
+++ src/main/java/category/model/Category.java
@@ -19,7 +19,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
 }
