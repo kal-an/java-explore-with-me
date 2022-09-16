007c49d4ddd26cabe36bb69eb4ba71edd5e8a824
Andrey <dev.andr@yandex.com>
2022-09-16 11:10:35 +0500

    fix: change method toShortDto, toFullDto in event mapper

--- src/main/java/event/EventMapper.java
+++ src/main/java/event/EventMapper.java
@@ -5,11 +5,12 @@ import category.CategoryMapper;
 import event.dto.EventFullDto;
 import event.dto.EventShortDto;
 import event.model.Event;
+import event.model.EventExtended;
 import user.UserMapper;
 
 public class EventMapper {
 
-    public static EventShortDto toShortDto(Event event) {
+    public static EventShortDto toShortDto(EventExtended event) {
         return EventShortDto.builder()
                 .id(event.getId())
                 .annotation(event.getAnnotation())
@@ -18,10 +19,11 @@ public class EventMapper {
                 .initiator(UserMapper.toDto(event.getInitiator()))
                 .paid(event.getPaid())
                 .title(event.getTitle())
+                .confirmedRequests(event.getRequestCount())
                 .build();
     }
 
-    public static EventFullDto toFullDto(Event event) {
+    public static EventFullDto toFullDto(EventExtended event) {
         final EventFullDto dto = new EventFullDto();
         var location = EventFullDto.Location.builder()
                 .lat(event.getLat())
@@ -31,7 +33,7 @@ public class EventMapper {
         dto.setAnnotation(event.getAnnotation());
         dto.setCategory(CategoryMapper.toDto(event.getCategory()));
         dto.setEventDate(event.getEventDate());
-        dto.setInitiator(UserMapper.toDto(event.getInitiator()));
+        dto.setInitiator(UserMapper.toShortDto(event.getInitiator()));
         dto.setPaid(event.getPaid());
         dto.setTitle(event.getTitle());
         dto.setDescription(event.getDescription());
@@ -41,6 +43,7 @@ public class EventMapper {
         dto.setRequestModeration(event.getRequestModeration());
         dto.setState(event.getState().name());
         dto.setLocation(location);
+        dto.setConfirmedRequests(event.getRequestCount());
         return dto;
     }
 
