05bfc9d0c252d0ef43a7caf2450b954a2c6a953a
Andrey <dev.andr@yandex.com>
2022-09-16 11:11:03 +0500

    feat: added new methods to event service interface

--- src/main/java/event/EventService.java
+++ src/main/java/event/EventService.java
@@ -1,9 +1,6 @@
 package event;
 
-import event.dto.EventFullDto;
-import event.dto.EventShortDto;
-import event.dto.NewEventDto;
-import event.dto.UpdateEventRequest;
+import event.dto.*;
 import user.model.User;
 
 import java.util.List;
@@ -15,14 +12,25 @@ public interface EventService {
                                      Boolean onlyAvailable, String sort,
                                      Integer from, Integer size);
 
+    List<EventFullDto> getAllEvents(List<Integer> users, List<String> states,
+                                    List<Integer> categories,
+                                    String rangeStart, String rangeEnd,
+                                    Integer from, Integer size);
+
     EventFullDto getEvent(Integer id);
 
     List<EventShortDto> getUserEvents(Integer userId, Integer authUser,
                                       Integer from, Integer size);
 
-    EventFullDto updateEvent(UpdateEventRequest dto);
+    EventFullDto updateEvent(UpdateEventRequest updateDto);
+
+    EventFullDto updateEvent(Integer eventId, AdminUpdateEventRequest updateDto);
 
     EventFullDto createEvent(NewEventDto dto, User user);
 
     EventFullDto cancelEvent(Integer eventId);
+
+    EventFullDto publishEvent(Integer eventId);
+
+    EventFullDto rejectEvent(Integer eventId);
 }
