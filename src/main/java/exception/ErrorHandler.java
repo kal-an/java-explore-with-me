547402b9154b118c1814c5889b07e3fa5da3e2cb
Andrey <dev.andr@yandex.com>
2022-09-16 11:19:23 +0500

    feat: added error handler class

---
+++ src/main/java/exception/ErrorHandler.java
@@ -0,0 +1,20 @@
+package exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import user.UserController;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice(assignableTypes = {
+        UserController.class})
+public class ErrorHandler {
+
+    @ExceptionHandler({ForbiddenException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ApiError handleForbidden(final RuntimeException e) {
+        return new ApiError(e.getStackTrace(), e.getMessage(), LocalDateTime.now());
+    }
+}
