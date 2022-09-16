1d5b8d2ec7d13845bee708094a6b4866417c3d1a
Andrey <dev.andr@yandex.com>
2022-09-16 10:22:49 +0500

    feat: added field events to compilation

--- src/main/java/compilation/model/Compilation.java
+++ src/main/java/compilation/model/Compilation.java
@@ -1,8 +1,10 @@
 package compilation.model;
 
+import event.model.Event;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Setter
 @Getter
@@ -18,6 +20,10 @@ public class Compilation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "event_id")
+    private Set<Event> events;
 
     @Column(nullable = false)
     private Boolean pinned;
