23676ba5790cdea171c7f2aa580e39cd734a6349
Andrey <dev.andr@yandex.com>
2022-09-16 18:06:59 +0500

    feat: added constructor to event extended

--- src/main/java/ru/yandex/practicum/event/model/EventExtended.java
+++ src/main/java/ru/yandex/practicum/event/model/EventExtended.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.yandex.practicum.category.model.Category;
+import ru.yandex.practicum.user.model.User;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -11,5 +15,15 @@ import lombok.Setter;
 @NoArgsConstructor
 public class EventExtended extends Event {
 
-    private Integer requestCount;
+    private Long requestCount;
+
+    public EventExtended(Integer id, String annotation, Category category, String eventDate,
+                         User initiator, Boolean paid, String title,
+                         String description, LocalDateTime createdOn, Integer participantLimit,
+                         LocalDateTime publishedOn, Boolean requestModeration, State state,
+                         Double lat, Double lon, Long requestCount) {
+        super(id, annotation, category, eventDate, initiator, paid, title, description, createdOn,
+                participantLimit, publishedOn, requestModeration, state, lat, lon);
+        this.requestCount = requestCount;
+    }
 }
