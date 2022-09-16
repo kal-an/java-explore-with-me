176312dd6896fb8e1d4a370d7d21e06b193c9d37
Andrey <dev.andr@yandex.com>
2022-09-16 18:07:23 +0500

    fix: compilation repository query

--- src/main/java/ru/yandex/practicum/compilation/CompilationRepository.java
+++ src/main/java/ru/yandex/practicum/compilation/CompilationRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Integer> {
 
-    @Query("select c from Compilations c where c.pinned is ?1")
+    @Query("select c from Compilation c where c.pinned = ?1")
     List<Compilation> findAllByPinned(Boolean pinned, Pageable pageable);
 }
