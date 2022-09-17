428ed84e2338337eb766b49438e1ce3cecd40404
Andrey <dev.andr@yandex.com>
2022-09-17 10:54:16 +0500

    feat: added validation in getEvents in user service

--- src/main/java/ru/yandex/practicum/user/UserService.java
+++ src/main/java/ru/yandex/practicum/user/UserService.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.event.dto.NewEventDto;
 import ru.yandex.practicum.event.dto.UpdateEventRequest;
 import ru.yandex.practicum.request.dto.ParticipationRequestDto;
 
+import javax.validation.constraints.Min;
 import java.util.List;
 
 public interface UserService {
@@ -16,7 +17,7 @@ public interface UserService {
 
     ParticipationRequestDto cancelRequest(Integer userId, Integer requestId);
 
-    List<EventShortDto> getEvents(Integer userId, Integer from, Integer size);
+    List<EventShortDto> getEvents(Integer userId, @Min(0) Integer from, @Min(1) Integer size);
 
     EventFullDto updateEvent(Integer userId, UpdateEventRequest updateDto);
 
