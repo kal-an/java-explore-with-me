545ff2965b1d7801b7a9297fe00c9b9a23a8e9e4
Andrey <dev.andr@yandex.com>
2022-09-20 00:14:14 +0500

    fix: change event mapper to build DTO

--- src/main/java/ru/yandex/practicum/event/EventMapper.java
+++ src/main/java/ru/yandex/practicum/event/EventMapper.java
@@ -2,11 +2,12 @@ package ru.yandex.practicum.event;
 
 
 import ru.yandex.practicum.category.CategoryMapper;
+import ru.yandex.practicum.category.dto.CategoryDto;
 import ru.yandex.practicum.event.dto.EventFullDto;
 import ru.yandex.practicum.event.dto.EventShortDto;
 import ru.yandex.practicum.event.model.Event;
-import ru.yandex.practicum.event.model.EventExtended;
-import ru.yandex.practicum.user.UserMapper;
+import ru.yandex.practicum.event.model.EventWithRequestsViews;
+import ru.yandex.practicum.user.dto.UserShortDto;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,20 +16,26 @@ public class EventMapper {
 
     private static final DateTimeFormatter DF = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static EventShortDto toShortDto(EventExtended event) {
+    public static EventShortDto toShortDto(EventWithRequestsViews event) {
         return EventShortDto.builder()
                 .id(event.getId())
                 .annotation(event.getAnnotation())
-                .category(CategoryMapper.toDto(event.getCategory()))
+                .category(CategoryDto.builder()
+                        .id(event.getCategory().getId())
+                        .name(event.getCategory().getName())
+                        .build())
                 .eventDate(event.getEventDate().format(DF))
-                .initiator(UserMapper.toDto(event.getInitiator()))
+                .initiator(UserShortDto.builder()
+                        .id(event.getInitiator().getId())
+                        .name(event.getInitiator().getName())
+                        .build())
                 .paid(event.getPaid())
                 .title(event.getTitle())
-                .confirmedRequests(event.getRequestCount())
+                .confirmedRequests(event.getRequests())
                 .build();
     }
 
-    public static EventFullDto toFullDto(EventExtended event) {
+    public static EventFullDto toFullDto(EventWithRequestsViews event) {
         final EventFullDto dto = new EventFullDto();
         var location = EventFullDto.Location.builder()
                 .lat(event.getLat())
@@ -36,19 +43,26 @@ public class EventMapper {
                 .build();
         dto.setId(event.getId());
         dto.setAnnotation(event.getAnnotation());
-        dto.setCategory(CategoryMapper.toDto(event.getCategory()));
+        dto.setCategory(CategoryDto.builder()
+                .id(event.getCategory().getId())
+                .name(event.getCategory().getName())
+                .build());
         dto.setEventDate(event.getEventDate().format(DF));
-        dto.setInitiator(UserMapper.toShortDto(event.getInitiator()));
+        dto.setInitiator(UserShortDto.builder()
+                .id(event.getInitiator().getId())
+                .name(event.getInitiator().getName())
+                .build());
         dto.setPaid(event.getPaid());
         dto.setTitle(event.getTitle());
         dto.setDescription(event.getDescription());
-        dto.setCreatedOn(event.getCreatedOn());
+        dto.setCreatedOn(event.getCreatedOn().format(DF));
         dto.setParticipantLimit(event.getParticipantLimit());
-        dto.setPublishedOn(event.getPublishedOn());
+        dto.setPublishedOn(event.getPublishedOn() == null ? null : event.getPublishedOn().format(DF));
         dto.setRequestModeration(event.getRequestModeration());
         dto.setState(event.getState().name());
         dto.setLocation(location);
-        dto.setConfirmedRequests(event.getRequestCount());
+        dto.setViews(event.getViews());
+        dto.setConfirmedRequests(event.getRequests());
         return dto;
     }
 
@@ -61,9 +75,9 @@ public class EventMapper {
                 .paid(dto.getPaid())
                 .title(dto.getTitle())
                 .description(dto.getDescription())
-                .createdOn(dto.getCreatedOn())
+                .createdOn(LocalDateTime.parse(dto.getCreatedOn(), DF))
                 .participantLimit(dto.getParticipantLimit())
-                .publishedOn(dto.getPublishedOn())
+                .publishedOn(LocalDateTime.parse(dto.getPublishedOn(), DF))
                 .requestModeration(dto.getRequestModeration())
                 .lat(dto.getLocation().getLat())
                 .lon(dto.getLocation().getLon())
