48fe1a35c3dc287664eae2a6c3db856326164785
Andrey <dev.andr@yandex.com>
2022-09-25 20:57:01 +0500

    feat: added interface stat service

--- stat-service/src/main/java/ru/yandex/practicum/stats/StatsService.java
+++ stat-service/src/main/java/ru/yandex/practicum/stats/StatsService.java
@@ -1,6 +1,13 @@
 package ru.yandex.practicum.stats;
 
+import ru.yandex.practicum.stats.dto.EndpointHit;
+import ru.yandex.practicum.stats.dto.ViewStats;
+
+import java.util.List;
+
 public interface StatsService {
 
+    List<ViewStats> getStats(String start, String end, List<String> uris, Boolean unique);
 
+    void addHit(EndpointHit newDto);
 }
