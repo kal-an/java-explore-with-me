a675e10faed155e275c946fdcd5f90e43161b493
Andrey <dev.andr@yandex.com>
2022-09-26 13:10:59 +0500

    feat: added annotation to DTO viewstat

--- src/main/java/ru/yandex/practicum/event/client/dto/ViewStats.java
+++ src/main/java/ru/yandex/practicum/event/client/dto/ViewStats.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.event.client.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ViewStats {
 
     private String app;
