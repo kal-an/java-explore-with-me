61a6744b66c0a4b75c2e3fc29cb3ab712a1d658d
Andrey <dev.andr@yandex.com>
2022-09-26 17:51:23 +0500

    feat: added new method to exception handler

--- src/main/java/ru/yandex/practicum/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/exception/ErrorHandler.java
@@ -4,17 +4,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.admin.AdminController;
+import ru.yandex.practicum.category.CategoryController;
+import ru.yandex.practicum.category.CategoryNotFoundException;
+import ru.yandex.practicum.compilation.CompilationController;
+import ru.yandex.practicum.compilation.CompilationNotFoundException;
+import ru.yandex.practicum.event.EventController;
+import ru.yandex.practicum.event.EventNotFoundException;
+import ru.yandex.practicum.request.RequestNotFoundException;
 import ru.yandex.practicum.user.UserController;
+import ru.yandex.practicum.user.UserNotFoundException;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice(assignableTypes = {
-        UserController.class})
+        UserController.class,
+        AdminController.class,
+        CategoryController.class,
+        CompilationController.class,
+        EventController.class})
 public class ErrorHandler {
 
     @ExceptionHandler({ForbiddenException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ApiError handleForbidden(final RuntimeException e) {
+        return new ApiError(e.getStackTrace(), e.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler({
+            CategoryNotFoundException.class,
+            CompilationNotFoundException.class,
+            EventNotFoundException.class,
+            RequestNotFoundException.class,
+            UserNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleNotFound(final RuntimeException e) {
         return new ApiError(e.getStackTrace(), e.getMessage(), LocalDateTime.now());
     }
 }
