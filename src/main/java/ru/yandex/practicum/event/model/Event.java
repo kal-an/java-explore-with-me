a24b080de7be61c694d82c4c3574aaa6c981555c
Andrey <dev.andr@yandex.com>
2022-09-26 18:11:29 +0500

    feat: added field compilations to Event

--- src/main/java/ru/yandex/practicum/event/model/Event.java
+++ src/main/java/ru/yandex/practicum/event/model/Event.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.event.model;
 
 import lombok.*;
 import ru.yandex.practicum.category.model.Category;
+import ru.yandex.practicum.compilation.model.Compilation;
 import ru.yandex.practicum.request.model.Request;
 import ru.yandex.practicum.user.model.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Setter
 @Getter
@@ -70,5 +72,12 @@ public class Event {
 
     @OneToMany(mappedBy = "event")
     private List<Request> requests;
+
+    @ManyToMany
+    @JoinTable(
+            name = "events_compilations",
+            joinColumns = @JoinColumn(name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "compilation_id"))
+    private Set<Compilation> compilations;
 
 }
