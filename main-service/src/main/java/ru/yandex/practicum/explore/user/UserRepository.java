22309a6de5bf7b192c1c7a39dc30b07e87f62434
Andrey <dev.andr@yandex.com>
2022-09-28 22:00:21 +0500

    fix: added query annotation to user repository

--- main-service/src/main/java/ru/yandex/practicum/explore/user/UserRepository.java
+++ main-service/src/main/java/ru/yandex/practicum/explore/user/UserRepository.java
@@ -2,11 +2,13 @@ package ru.yandex.practicum.explore.user;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.yandex.practicum.explore.user.model.User;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @Query("select u from User u")
     List<User> findAllUsers(Pageable pageable);
 }
