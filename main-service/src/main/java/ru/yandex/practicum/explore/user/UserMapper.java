be3ac99c6aef7336b0a124f338cb2b7fd83ea2a1
Andrey <dev.andr@yandex.com>
2022-10-01 14:21:15 +0500

    feat: added annotation to user mapper, added method to convert into list

--- main-service/src/main/java/ru/yandex/practicum/explore/user/UserMapper.java
+++ main-service/src/main/java/ru/yandex/practicum/explore/user/UserMapper.java
@@ -1,9 +1,15 @@
 package ru.yandex.practicum.explore.user;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import ru.yandex.practicum.explore.user.dto.UserDto;
 import ru.yandex.practicum.explore.user.dto.UserShortDto;
 import ru.yandex.practicum.explore.user.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
 
     public static UserShortDto toShortDto(User user) {
@@ -27,5 +33,13 @@ public class UserMapper {
                 .name(dto.getName())
                 .email(dto.getEmail())
                 .build();
+    }
+
+    public static List<UserDto> toDtoList(Iterable<User> users) {
+        List<UserDto> dtos = new ArrayList<>();
+        for (User user : users) {
+            dtos.add(toDto(user));
+        }
+        return dtos;
     }
 }
