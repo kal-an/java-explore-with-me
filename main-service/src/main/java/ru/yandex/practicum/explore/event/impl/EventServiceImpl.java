9e47441002b790085f45e3fdfcbc48bbfb6096b7
Andrey <dev.andr@yandex.com>
2022-10-04 20:31:49 +0500

    feat: added annotation Validated in Event Service

--- main-service/src/main/java/ru/yandex/practicum/explore/event/impl/EventServiceImpl.java
+++ main-service/src/main/java/ru/yandex/practicum/explore/event/impl/EventServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import ru.yandex.practicum.explore.category.CategoryMapper;
 import ru.yandex.practicum.explore.category.CategoryRepository;
 import ru.yandex.practicum.explore.category.model.Category;
@@ -27,6 +28,7 @@ import java.util.List;
 
 @Service
 @Slf4j
+@Validated
 public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
