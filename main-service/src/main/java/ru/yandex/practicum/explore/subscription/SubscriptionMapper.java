3c29fc9b22d8d349c2cf0b16603371cb643435ba
Andrey <dev.andr@yandex.com>
2022-10-04 15:59:03 +0500

    feat: added method toDtoList in SubscriptionMapper

--- main-service/src/main/java/ru/yandex/practicum/explore/subscription/SubscriptionMapper.java
+++ main-service/src/main/java/ru/yandex/practicum/explore/subscription/SubscriptionMapper.java
@@ -4,6 +4,9 @@ import ru.yandex.practicum.explore.subscription.dto.SubscriptionDto;
 import ru.yandex.practicum.explore.subscription.model.Subscription;
 import ru.yandex.practicum.explore.user.UserMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubscriptionMapper {
 
     public static SubscriptionDto toDto(Subscription subscription) {
@@ -11,5 +14,13 @@ public class SubscriptionMapper {
                 .id(subscription.getId())
                 .user(UserMapper.toShortDto(subscription.getUser()))
                 .build();
+    }
+
+    public static List<SubscriptionDto> toDtoList(Iterable<Subscription> subscriptions) {
+        List<SubscriptionDto> dtos = new ArrayList<>();
+        for (Subscription subscription : subscriptions) {
+            dtos.add(toDto(subscription));
+        }
+        return dtos;
     }
 }
